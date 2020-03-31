67a4456841d0525a215994285dbb642d6ac160f4
gmolin <gmolin@student.hive.fi>
2020-03-31 18:04:30 +0300

    d01/01: listening events with correct length of description and time formating as seen in UI/UX

--- src/app/events/events-list/event-item/event-item.component.ts
+++ src/app/events/events-list/event-item/event-item.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpService } from '../../../http.service';
+import moment from "moment"; moment().format;
 
 @Component({
   selector: 'app-event-item',
   templateUrl: './event-item.component.html',
   styleUrls: ['./event-item.component.css'],
 })
+
 export class EventItemComponent implements OnInit {
 
-  @Input() _events: Object;
+  @Input() events: any;
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.events.begin_at = moment(this.events.begin_at).format('ddd, MMM D • YYYY @ hh:mm A');
   }
 }
